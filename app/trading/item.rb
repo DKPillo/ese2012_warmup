51ee703de7816b8382911a9fa44d2f3b76d9e704
Salim Hermidas <s.hermidas@gmail.com>
2012-09-25 01:03:16 +0200

    most of the methods ar now implemented. now we need some good tests and maybe some small modifications.

--- app/trading/item.rb
+++ app/trading/item.rb
@@ -7,15 +7,26 @@ module Trading
     #An item has an owner.
 
     # generate getter and setter for name and price
-    attr_accessor :name, :price, :active
+    attr_accessor :name, :price, :owner, :active
 
     # factory method (constructor) on the class
-    def self.created( name, price )
+    def self.created( name, price, owner )
       item = self.new
       item.name = name
       item.price = price
       item.active = false
+      item.owner = owner
       item
+    end
+
+    # get state
+    def is_active?
+      self.active
+    end
+
+    # set owner
+    def set_owner(new_owner)
+      self.owner = new_owner
     end
 
     # to String-method
@@ -29,6 +40,11 @@ module Trading
       self.active = true
     end
 
+    # to set inactive
+    def to_inactive
+      self.active = false
+    end
+
     # get name
     def get_name
       # string interpolation
@@ -38,7 +54,7 @@ module Trading
     # get price
     def get_price
       # int interpolation
-      "#{price}"
+      self.price
     end
 
   end
--- test/user_test.rb
+++ test/user_test.rb
@@ -2,10 +2,10 @@ require 'test/unit'
 require '../app/trading/item'
 require '../app/trading/user'
 
-class ItemTest < Test::Unit::TestCase
+class UserTest < Test::Unit::TestCase
 
   # Fake test
-  def test_item_create_by_user
+  def test_user_item_create
     owner = Trading::User.created( "testuser" )
     assert( owner.list_items.size == 0, "Item list length should be 0" )
     assert( owner.list_items_inactive.size == 0, "Item list inactive length should be 0" )
@@ -18,6 +18,13 @@ class ItemTest < Test::Unit::TestCase
     assert( owner.list_items_inactive.size == 0, "Item list inactive length should be 0" )
     assert( owner.list_items[0].is_active? , "New created item should now be active" )
     assert( owner.list_items[0].to_s, "testobject, 10" )
+  end
+
+  def test_create_user
+    owner = Trading::User.created( "testuser" )
+    assert( owner.get_name == "testuser", "Name should be correct")
+    assert( owner.get_credits == 100, "Credits should be 100 first")
+    assert( owner.to_s == "testuser has currently 100 credits, 0 active and 0 inactive items", "String representation is wrong generated")
   end
 
 end
